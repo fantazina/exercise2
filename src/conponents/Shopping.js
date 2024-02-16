00458fb4da6a6b6db42023f3a5a5c0fbd398b4ea
fantazina <137284904+fantazina@users.noreply.github.com>
2024-02-16 19:34:32 +0900

    장보기 목록 최종

--- src/conponents/Shopping.js
+++ src/conponents/Shopping.js
@@ -6,6 +6,7 @@ const Shopping = () => {
     const [inputItem, setInputItem] = useState('')
     const [shoppingList, setShoppingList] = useState([])
     const inputRef = useRef(null)
+    const [ListReset, setListReset] = useState('')
   
     const onInputItem = (e) => {
       const{ value } = e.target //입력할 value값 가져오기
@@ -27,13 +28,22 @@ const Shopping = () => {
         alert('추가 내용 입력하기')
       }
     }
+
+    const onListReset = () => {
+        onShoppingList([])
+    }
   
       return (
         <div className={ styles.S_list }>
             <input type='text' value={ inputItem } onChange={ onInputItem } ref={ inputRef } />
             <button onClick={ addItem }>추가</button>
             <ShoppingList shoppingList={ shoppingList } onShoppingList={ onShoppingList } />
-            <button>취소</button>
+
+            { shoppingList.length > 0 && 
+                <div className={ styles.S_list }> 
+                    <button className={ styles.reset_btn } onClick={ onListReset }>취소</button>
+                </div>
+            }
         </div>
     );
   };
